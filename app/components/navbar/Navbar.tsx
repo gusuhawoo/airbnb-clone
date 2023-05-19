ba97a7ca3eff75cce973e071966e2c9d2fb39cdd
suha <gusuhawoo@gmail.com>
2023-05-19 21:48:03 +0900

    Add : first setup project

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div>this is</div>
+      Nabar!
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      Airbnb
+      <h1 className="text-rose-500 text-2xl">Airbnb</h1>
+    </div>
+  );
+}
